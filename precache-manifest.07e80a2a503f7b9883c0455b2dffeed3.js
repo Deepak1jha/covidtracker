98f6f8fbc2bf6dd0ba18ec99fb22dec7f9bcb062
Deepak jha <ddjha171@gmail.com>
2020-07-15 19:57:28 +0530

    Updates

---
+++ precache-manifest.07e80a2a503f7b9883c0455b2dffeed3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab61045cce2fd35f1d58b3af1d184e1a",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "22285910231fc2eca66a",
+    "url": "/covidtracker/static/css/2.c5feca96.chunk.css"
+  },
+  {
+    "revision": "cef815d3ba20e6d8d227",
+    "url": "/covidtracker/static/css/main.422691e9.chunk.css"
+  },
+  {
+    "revision": "22285910231fc2eca66a",
+    "url": "/covidtracker/static/js/2.e1e875e9.chunk.js"
+  },
+  {
+    "revision": "e23fabadb975545b74e4a8a97216a709",
+    "url": "/covidtracker/static/js/2.e1e875e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cef815d3ba20e6d8d227",
+    "url": "/covidtracker/static/js/main.5355da3c.chunk.js"
+  },
+  {
+    "revision": "83995dd3236b8694739e",
+    "url": "/covidtracker/static/js/runtime-main.a11e111e.js"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/covidtracker/static/media/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/covidtracker/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/covidtracker/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/covidtracker/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/covidtracker/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/covidtracker/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/covidtracker/static/media/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/covidtracker/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/covidtracker/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/covidtracker/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/covidtracker/static/media/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/covidtracker/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/covidtracker/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/covidtracker/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/covidtracker/static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/covidtracker/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/covidtracker/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/covidtracker/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/covidtracker/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/covidtracker/static/media/nucleo-icons.f82ec6ba.ttf"
+  }
+]);
